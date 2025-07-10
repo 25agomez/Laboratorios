96e4571d742400b1b159538e3d10e9f1e9446237
Andres Gomez <andalli2508@gmail.com.com>
2025-07-09 23:29:07 -0600

    Add Lab06 files: create db.php, index.php, lab6-test.php, and procesar.php; update index.html

---
+++ lab06/lab6-test.php
@@ -0,0 +1,21 @@
+<?php
+$mensaje = "Hello, World!";
+$suma = 2 + 3;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab06</title>
+</head>
+<body>
+    <?php
+    //Mostrar el mensaje
+    echo "<h1>$mensaje</h1>";
+    //Mostrar la suma
+    echo "<p>La suma de 2 + 3 es: $suma</p>";
+    ?>
+</body>
+</html>
